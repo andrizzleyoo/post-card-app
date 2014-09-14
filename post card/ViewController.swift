5c3697034884d2225e98186c382482e40f315c1f
Imagination Overdrive <io@Imaginations-MacBook-Pro.local>
2014-09-13 21:25:56 -0400

    Initial Commit

---
+++ post card/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  post card
+//
+//  Created by Imagination Overdrive, Inc. on 9/13/14.
+//  Copyright (c) 2014 Imagination Overdrive, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
